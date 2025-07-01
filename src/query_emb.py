f78e15d94dd997713822fa2c4e85cca57173bf7e
b22cse040 <b22cs040@iitj.ac.in>
2025-07-01 18:53:17 +0530

    Added fuzzy keyword search to improve search results

--- src/query_emb.py
+++ src/query_emb.py
@@ -17,19 +17,77 @@ es = Elasticsearch(
 MODEL_NAME = "all-MiniLM-L6-v2"
 
 model = SentenceTransformer(MODEL_NAME)
-query_text = "headphones"
-query_embedding = model.encode(query_text, normalize=True)
+def search_elasticsearch_embedding(query: str, top_k: int = 5) -> list[dict]:
+  '''
+  Performs kNN search on Elasticsearch using dense embeddings.
 
-knn_query = {
-  "knn" : {
-    "field" : "embedding",
-    "query_vector" : query_embedding.tolist(),
-    "k" : 10,
-    "num_candidates" : 100
+  Args:
+    query_text (str) : The text query to search for.
+    top_k (int) : The number of top results to return.
+
+  Returns:
+    List of dictionaries containing the top k results.
+  '''
+  query_embedding = model.encode(query)
+  knn_query = {
+    "knn" : {
+      "field" : "embedding",
+      "query_vector" : query_embedding.tolist(),
+      "k" : top_k,
+      "num_candidates" : max(10 * top_k, 100),
+    }
   }
-}
 
-response = es.search(index="corpus_chunks", body={"query": knn_query}, source=["content"])
+  response = es.search(
+    index="corpus_chunks",
+    body=knn_query,
+    source=["content"]
+  )
 
-for hit in response['hits']['hits']:
-  print(hit["_source"]["content"], "| Score: ", hit["_score"])
+  results = []
+  for hit in response["hits"]["hits"]:
+    results.append({
+      "content" : hit["_source"]["content"],
+      "score" : hit["_score"],
+    })
+
+  return results
+
+def search_es_keywords(query: str, top_k: int = 5) -> list[dict]:
+  keyword_query = {
+    "query" : {
+      "match" : {
+        "content" : {
+          "query" : query,
+          # "operator": "and",
+          "fuzziness" : "AUTO",
+        }
+      }
+    },
+    "size" : top_k,
+  }
+
+  response = es.search(index="corpus_chunks", body=keyword_query)
+  results = []
+
+  for hit in response["hits"]["hits"]:
+    results.append({
+      "content" : hit["_source"]["content"],
+      "score" : hit["_score"],
+    })
+
+  return results
+
+def search_query(query: str, top_k: int = 5) -> list[dict]:
+  semantic_hits = search_elasticsearch_embedding(query, top_k)
+  keywords_hits = search_es_keywords(query, top_k)
+  top_hits = semantic_hits + keywords_hits
+  return top_hits
+
+def form_response(top_hits: list[dict], model: str) -> str:
+  pass
+
+if __name__ == "__main__":
+  hits = search_query("headpnhoes", top_k=3)
+  for hit in hits:
+    print(f"{hit['content']}\n{hit["score"]}")
