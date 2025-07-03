0709a59029a5319f15fadb388359605d9f489542
b22cse040 <b22cs040@iitj.ac.in>
2025-07-03 18:25:48 +0530

    Optimized retrieval by approx 45%

--- src/query_emb.py
+++ src/query_emb.py
@@ -1,7 +1,8 @@
-import os
+import os, time
 from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
 from elasticsearch import Elasticsearch
+from concurrent.futures import ThreadPoolExecutor
 
 load_dotenv()
 
@@ -82,7 +83,7 @@ def search_es_keywords(query: str, top_k: int = 5) -> list[dict]:
 
 def search_query(query: str, top_k: int = 5) -> list[dict]:
   """
-  Find the most relevant keyword and semantic hits for thw query.
+  Find the most relevant keyword and semantic hits for thw query in parallel.
   Args:
     query (str): The query to search for.
     top_k: Gives the top k results for the query.
@@ -91,9 +92,24 @@ def search_query(query: str, top_k: int = 5) -> list[dict]:
     A concatenated list of top k results of both searches, yielding a lisk of
     size 2k.
   """
-  semantic_hits = search_elasticsearch_embedding(query, top_k)
-  keywords_hits = search_es_keywords(query, top_k)
-  top_hits = semantic_hits + keywords_hits
+  start_time = time.time()
+  # semantic_hits = search_elasticsearch_embedding(query, top_k)
+  # keywords_hits = search_es_keywords(query, top_k)
+  # top_hits = semantic_hits + keywords_hits
+
+  with ThreadPoolExecutor(max_workers=2) as executor:
+    future_semantic = executor.submit(search_elasticsearch_embedding, query, top_k)
+    future_keywords = executor.submit(search_es_keywords, query, top_k)
+
+    semantic_hits = future_semantic.result()
+    keyword_hits = future_keywords.result()
+
+  top_hits = semantic_hits + keyword_hits
+  total_time = time.time() - start_time
+  file_path = "../evals/search_query.txt"
+
+  with open(file_path, "a") as f:
+    f.write(f"Got {len(top_hits)} hits in {total_time:.4f} seconds for the query: {query} [PARALLEL]\n")
   return top_hits
 
 def search_similar_queries(query: str, top_k: int = 2) -> list[dict]:
@@ -137,6 +153,13 @@ def search_similar_queries(query: str, top_k: int = 2) -> list[dict]:
   return results
 
 if __name__ == "__main__":
-  hits = search_query("sunfolwer", top_k=3)
-  for hit in hits:
-    print(f"{hit['content']}\nScore: {hit['score']:.4f}\n{'='*50}")
+  queries = ["sunflowers", "bedshits and mattersess", "Headphones", "wine bar", "wall art"]
+
+  for query in queries:
+    hits = search_query(query, top_k=10)
+    # for hit in hits:
+    #   print(f"{hit['content']}\nScore: {hit['score']:.4f}\n{'='*50}")
+
+  ## Average time for parallel: 0.0589 sec, check evals directory
+  ## Average time for sequentially: 0.1068 sec, check evals directory
+  ## Optimization: 44.85% optimization in time
